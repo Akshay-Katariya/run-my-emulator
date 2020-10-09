62df204794fd047dd4837c4017558ad7e66ee2b3
Akshay-Katariya <akshay.katariya1@gmail.com>
2020-10-09 17:15:54 +0530

    Added run script

---
+++ run.sh
@@ -0,0 +1,26 @@
+ALL_EMULATORS=( `emulator -list-avds` )
+ALL_EMULATORS_length=${#ALL_EMULATORS[@]}
+
+echo "\n----------SELECT AVD----------"
+
+INDEX=0
+for element in "${ALL_EMULATORS[@]}"
+do
+   echo "\n${INDEX}) $element"
+   let INDEX=${INDEX}+1
+done
+
+echo "\n------------------------------"
+
+
+echo "\nEnter ADV number which you want to use?"
+read varname
+
+if [[ "$varname" -lt "$ALL_EMULATORS_length"  && "$varname" -ge 0 ]]
+then
+  echo "\nStarting Simulator for you... 😃"
+  echo "\n🔴 Closing the terminal will close your Emulator 🔴"
+  run_avd = `emulator -avd ${ALL_EMULATORS[varname]}`
+else
+  echo "\nSelect valid AVD..."
+fi  
